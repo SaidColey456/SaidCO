63335db596232fc113bfd12ec82abebd8c05bc68
SaidColey456 <168228442+SaidColey456@users.noreply.github.com>
2024-10-21 14:19:39 -0500

    Update Actividad_2_de_Octubre.dart

--- Said_Coley_Romero/Programación Orientada a Objetos/Actividad_2_de_Octubre/Actividad_2_de_Octubre.dart
+++ Said_Coley_Romero/Programación Orientada a Objetos/Actividad_2_de_Octubre/Actividad_2_de_Octubre.dart
@@ -14,26 +14,25 @@ class User {
   User(Map m) {
     this.id = m['id'];
     this.name = m['name'];
-    this.username = m['username'];  
+    this.username = m['username'];
     this.email = m['email'];
     this.website = m['website'];
     this.phone = m['phone'];
-    
+
     Map company = m['company'];
     this.company = Company(company);
 
     Map address = m['address'];
     this.address = Address(address);
-
   }
-} 
+}
 
 class Company {
   String? name;
   String? catchPhrase;
   String? bs;
 
-  Company(Map m){
+  Company(Map m) {
     this.name = m['name'];
     this.catchPhrase = m['catchPhrase'];
     this.bs = m['bs'];
@@ -47,35 +46,40 @@ class Address {
   String? zipcode;
   Geo? geo;
 
-  Address(Map m){
-    
+  Address(Map m) {
     this.street = m['street'];
     this.suite = m['suite'];
     this.city = m['city'];
     this.zipcode = m['zipcode'];
-    this.geo =  Geo(m['geo']);
-
+    this.geo = Geo(m['geo']);
   }
-
 }
 
 class Geo {
   String? lat;
   String? lng;
 
-  Geo(Map){
-    Map<String, dynamic>  m = jsonDecode(s);
+  Geo(Map m) {
     this.lat = m['lat'];
-    this. lng = m['lng'];
+    this.lng = m['lng'];
   }
 }
 
-void main(){
+void main() async {
   var url = Uri.https('jsonplaceholder.typicode.com', '/users/4');
   print('Calculando...');
   var response = await http.get(url);
-print('Response status: ${response.statusCode}')
-print('Response ')
-  
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 
+  if (response.statusCode == 200) {
+    Map<String, dynamic> data = jsonDecode(response.body);
+    User user = User(data);
+    print('User name: ${user.name}');
+    print('User email: ${user.email}');
+    print('Company name: ${user.company?.name}');
+    print('Address: ${user.address?.street}, ${user.address?.city}');
+  } else {
+    print('Failed to load user data');
+  }
 }
