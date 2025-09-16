699bf2bbe31e34c17023b50463319306eb68b79d
SaidColey456 <168228442+SaidColey456@users.noreply.github.com>
2025-09-16 17:00:16 -0500

    fix

--- Said_Coley_Romero/Actividad_flutter_01_09_2025/lib/feature/login/presentation/views/login_loading_view.dart
+++ Said_Coley_Romero/Actividad_flutter_01_09_2025/lib/feature/login/presentation/views/login_loading_view.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class LoginLoadingView extends StatelessWidget {
-  const LoginLoadingView({Key? key}) : super(key: key);
+  const LoginLoadingView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Color(0xFF7F00FF),
+      backgroundColor: Color(0xFFF7F0FF),
       body: Center(
         child: CircularProgressIndicator(
-          color: Colors.white,
+          color: Colors.deepPurple,
           strokeWidth: 6,
         ),
       ),
